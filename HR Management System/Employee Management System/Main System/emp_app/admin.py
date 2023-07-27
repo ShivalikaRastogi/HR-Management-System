dbdc7f6ca50ceeb5bd605a9af7145a16035b3cfd
ShivalikaRastogi <shivalika3002@gmail.com>
2023-07-27 20:12:20 +0530

    main

--- HR Management System/Employee Management System/Main System/emp_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Roles, Department
-# Register your models here.
-
-@admin.register(Employee, Roles, Department)
-class UniversalAdmin(admin.ModelAdmin):
-    def get_list_display(self, request):
-        return [field.name for field in self.model._meta.concrete_fields]
